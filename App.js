0efc39853c199babc67681a794ab76bf9a7a849f
Young <jiiiy0510o@gmail.com>
2022-04-30 23:55:52 +0900

    7일간 일기예보+아이콘

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from "react-native";
 import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-console.log(SCREEN_WIDTH);
+const API_KEY = "257f4e6c8fc561a25d61cb3c90b41002";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Rain: "rain",
+  Thunderstorm: "lightning",
+  Atmosphere: "cloudy-gusts",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState(null);
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -21,9 +29,14 @@ export default function App() {
     } = await Location.getCurrentPositionAsync();
     const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
     setCity(location[0].street);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric&lang=kr`
+    );
+    const json = await response.json();
+    setDays(json.daily);
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -37,22 +50,24 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27도</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27도</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27도</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27도</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="black" size="large" style={{ marginTop: 10 }}></ActivityIndicator>
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", width: "100%" }}
+              >
+                <Fontisto name={icons[day.weather[0].main]} size={64} color="black" />
+                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}°C</Text>
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -72,19 +87,24 @@ const styles = StyleSheet.create({
     fontSize: 42,
   },
   weather: {
+    marginTop: 40,
     fontSize: 27,
   },
   day: {
+    paddingHorizontal: 32,
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-end",
   },
   temp: {
-    marginTop: 66,
-    fontSize: 98,
+    fontSize: 88,
     fontWeight: "900",
   },
   description: {
-    marginTop: -20,
+    marginTop: 6,
     fontSize: 40,
+  },
+  tinyText: {
+    marginTop: -6,
+    fontSize: 22,
   },
 });
